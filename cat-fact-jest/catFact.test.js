066fee41941ab2704363bf374be42c4a7dd84db5
Urim Eom <eomurim1@byu.edu>
2026-09-21 01:13:15 -0600

    Test cat fact fetch

--- cat-fact-jest/catFact.test.js
+++ cat-fact-jest/catFact.test.js
@@ -5,3 +5,20 @@ test("new CatFact object has empty history", () => {
 
   expect(catFact.history()).toEqual([]);
 });
+
+test("add returns a cat fact", async () => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          data: ["Cats are cute."],
+        }),
+    })
+  );
+
+  const catFact = new CatFact();
+  const result = await catFact.add();
+
+  expect(result).toBe("Cats are cute.");
+  expect(catFact.history()).toEqual(["Cats are cute."]);
+});
